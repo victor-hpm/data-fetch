09bff0a5036fefc7a7a0010ecbba55900c8c91a3
Victor Peña <victor.hp.mtz@gmail.com>
2020-09-07 20:25:05 -0500

    fech funcionando dos metodos

--- src/DataFetch.js
+++ src/DataFetch.js
@@ -13,73 +13,68 @@ export class DataFetch extends LitElement {
 
     static get properties() {
         return {
-            title: { type: String },
-            counter: { type: Number },
-            infoapi: { type: Array }
-            // individual: { type: Array }
+            infoapi: { type: Array },
+            data: { type: Array }
         };
     }
 
     constructor() {
         super();
-        this.title = 'Hey there';
-        this.counter = 5;
         this.infoapi = [];
-        // this.individual = [];
+        this.data = [];
     }
-
-    // firstUpdated() {
-    //     fetch('https://rickandmortyapi.com/api/character/')
-    //         .then(response => response.json())
-    //         .then(response => {
-    //             this.infoapi = response.results;
-    //             console.log('infoapi', this.infoapi);
-    //             this.mapeo();
-    //             console.log('individual', this.individual)
-    //         })
-    // }
-
-    // mapeo() {
-    //     this.infoapi.forEach((image, id, name) => {
-    //         this.individual.push({
-    //             image: image.image,
-    //             index: id.id,
-    //             name: name.name
-    //         });
-    //     });
-    // }
-
-
 
     firstUpdated() {
         fetch('https://rickandmortyapi.com/api/character/')
             .then(response => response.json())
-            .then(infoapi => {
-                // console.log('infoapi character', infoapi);
-                // console.log('infoapi character results', infoapi.results);
-                this.mapeo(infoapi.results);
-                console.log('infoapi card', this.infoapi)
-            })
-        this.dispatchEvent(new CustomEvent('info', { detail: this.infoapi }));
+            .then(response => {
+                this.infoapi = response.results;
+                // console.log('infoapi', this.infoapi);
+                this.mapeo();
+                // console.log('data', this.data);
+            });
+        this.dispatchEvent(new CustomEvent('info', { detail: this.data }));
     }
 
-    mapeo(infoapi) {
-        infoapi.forEach(element => {
-            const card = {
-                image: element.image,
-                id: element.id,
-                name: element.name,
-                gender: element.gender
-            }
-            this.infoapi = [...this.infoapi, card]
+    mapeo() {
+        this.infoapi.forEach((cartoon) => {
+            this.data.push({
+                image: cartoon.image,
+                index: cartoon.id,
+                name: cartoon.name,
+                gender: cartoon.gender,
+                location: cartoon.location.name
+            });
         });
     }
 
 
+
+    // firstUpdated() {
+    //     fetch('https://rickandmortyapi.com/api/character/')
+    //         .then(response => response.json())
+    //         .then(infoapi => {
+    //             // console.log('infoapi character', infoapi);
+    //             // console.log('infoapi character results', infoapi.results);
+    //             this.mapeo(infoapi.results);
+    //             // console.log('infoapi card', this.infoapi);
+    //         })
+    //     this.dispatchEvent(new CustomEvent('info', { detail: this.infoapi }));
+    // }
+
+    // mapeo(infoapi) {
+    //     infoapi.forEach(element => {
+    //         const card = {
+    //             image: element.image,
+    //             id: element.id,
+    //             name: element.name,
+    //             gender: element.gender
+    //         }
+    //         this.infoapi = [...this.infoapi, card]
+    //     });
+    // }
+
     render() {
-        return html `
-      <h2>${this.title} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
-    `;
+        return html ``;
     }
 }
